9a693fa7e96c1e3b32b1d38a96c5901e2818cc60
turnervink <turner.vink@me.com>
2015-08-19 22:42:24 -0700

    removed weather and animations

--- src/mealtime.c
+++ src/mealtime.c
@@ -1,39 +1,9 @@
 #include <pebble.h>
-#include <localize.h>
-	
-#define KEY_CONDITIONS 0
-#define KEY_TEMPERATURE 1
-#define KEY_TEMPERATUREC 2
-#define KEY_SCALE 3
 
 static Window *s_main_window;
-static TextLayer *s_time_layer, *s_ampm_layer, *s_date_layer, *s_weather_layer, *s_temp_layer;
-static GFont s_time_font, s_date_font, s_weather_font, s_temp_font;
+static TextLayer *s_time_layer, *s_ampm_layer, *s_date_layer;
+static GFont s_time_font, s_date_font;
 static Layer *batt_layer;
-
-void on_animation_stopped(Animation *anim, bool finished, void *context) {
-    //Free the memory used by the Animation
-    property_animation_destroy((PropertyAnimation*) anim);
-}
- 
-void animate_layer(Layer *layer, GRect *start, GRect *finish, int duration, int delay) {
-    //Declare animation
-    PropertyAnimation *anim = property_animation_create_layer_frame(layer, start, finish);
- 
-    //Set characteristics
-    animation_set_duration((Animation*) anim, duration);
-    animation_set_delay((Animation*) anim, delay);
- 
-    //Set stopped handler to free memory
-    AnimationHandlers handlers = {
-        //The reference to the stopped handler is the only one in the array
-        .stopped = (AnimationStoppedHandler) on_animation_stopped
-    };
-    animation_set_handlers((Animation*) anim, handlers, NULL);
- 
-    //Start animation!
-    animation_schedule((Animation*) anim);
-}
 
 static void update_time() {
 	time_t temp = time(NULL);
@@ -42,220 +12,155 @@ static void update_time() {
 
 	if(hour >= 21) {
 		text_layer_set_text(s_ampm_layer, " ");
-		text_layer_set_text(s_time_layer, _("night"));
+		text_layer_set_text(s_time_layer, "night");
 		#ifdef PBL_COLOR
 			window_set_background_color(s_main_window, GColorBlack);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
 			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
 		#else
 			window_set_background_color(s_main_window, GColorBlack);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
 			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
 		#endif
 	} else if(hour >= 18) {
 		text_layer_set_text(s_ampm_layer, " ");
-		text_layer_set_text(s_time_layer, _("evening"));
+		text_layer_set_text(s_time_layer, "evening");
 		#ifdef PBL_COLOR
 			window_set_background_color(s_main_window, GColorOxfordBlue);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
 			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
 		#else
 			window_set_background_color(s_main_window, GColorBlack);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
 			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
 		#endif
 	} else if(hour >= 15) {
-		text_layer_set_text(s_ampm_layer, _("late"));
-		text_layer_set_text(s_time_layer, _("afternoon"));
+		text_layer_set_text(s_ampm_layer, "late");
+		text_layer_set_text(s_time_layer, "afternoon");
 		#ifdef PBL_COLOR
 			window_set_background_color(s_main_window, GColorSunsetOrange);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
 			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
 		#else
 			window_set_background_color(s_main_window, GColorWhite);
 			text_layer_set_text_color(s_ampm_layer, GColorBlack);
 			text_layer_set_text_color(s_time_layer, GColorBlack);
 			text_layer_set_text_color(s_date_layer, GColorBlack);
-			text_layer_set_text_color(s_temp_layer, GColorBlack);
-			text_layer_set_text_color(s_weather_layer, GColorBlack);
 		#endif
 	} else if(hour >= 12) {
 		text_layer_set_text(s_ampm_layer, " ");
-		text_layer_set_text(s_time_layer, _("afternoon"));
+		text_layer_set_text(s_time_layer, "afternoon");
 		#ifdef PBL_COLOR
 			window_set_background_color(s_main_window, GColorVividCerulean);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
 			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
 		#else
 			window_set_background_color(s_main_window, GColorWhite);
 			text_layer_set_text_color(s_ampm_layer, GColorBlack);
 			text_layer_set_text_color(s_time_layer, GColorBlack);
 			text_layer_set_text_color(s_date_layer, GColorBlack);
-			text_layer_set_text_color(s_temp_layer, GColorBlack);
-			text_layer_set_text_color(s_weather_layer, GColorBlack);
 		#endif
 	} else if(hour >= 9) {
-		text_layer_set_text(s_ampm_layer, _("late"));
-		text_layer_set_text(s_time_layer, _("morning"));
+		text_layer_set_text(s_ampm_layer, "late");
+		text_layer_set_text(s_time_layer, "morning");
 		#ifdef PBL_COLOR
 			window_set_background_color(s_main_window, GColorVividCerulean);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
 			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
 		#else
 			window_set_background_color(s_main_window, GColorWhite);
 			text_layer_set_text_color(s_ampm_layer, GColorBlack);
 			text_layer_set_text_color(s_time_layer, GColorBlack);
 			text_layer_set_text_color(s_date_layer, GColorBlack);
-			text_layer_set_text_color(s_temp_layer, GColorBlack);
-			text_layer_set_text_color(s_weather_layer, GColorBlack);
 		#endif
 	} else if(hour >= 6) {
 		text_layer_set_text(s_ampm_layer, " ");
-		text_layer_set_text(s_time_layer, _("morning"));
+		text_layer_set_text(s_time_layer, "morning");
 		#ifdef PBL_COLOR
 			window_set_background_color(s_main_window, GColorVividCerulean);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
 			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
 		#else
 			window_set_background_color(s_main_window, GColorWhite);
 			text_layer_set_text_color(s_ampm_layer, GColorBlack);
 			text_layer_set_text_color(s_time_layer, GColorBlack);
 			text_layer_set_text_color(s_date_layer, GColorBlack);
-			text_layer_set_text_color(s_temp_layer, GColorBlack);
-			text_layer_set_text_color(s_weather_layer, GColorBlack);
 		#endif
 	} else if(hour >= 3) {
-		text_layer_set_text(s_ampm_layer, _("early"));
-		text_layer_set_text(s_time_layer, _("morning"));
+		text_layer_set_text(s_ampm_layer, "early");
+		text_layer_set_text(s_time_layer, "morning");
 		#ifdef PBL_COLOR
 			window_set_background_color(s_main_window, GColorVeryLightBlue);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
-			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
+			text_layer_set_text_color(s_date_layer, GColorWhite);	
 		#else
 			window_set_background_color(s_main_window, GColorBlack);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
 			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
 		#endif
 	} else if(hour >= 0) {
 		text_layer_set_text(s_ampm_layer, " ");
-		text_layer_set_text(s_time_layer, _("midnight"));
+		text_layer_set_text(s_time_layer, "midnight");
 		#ifdef PBL_COLOR
 			window_set_background_color(s_main_window, GColorBlack);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
 			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
 		#else
 			window_set_background_color(s_main_window, GColorBlack);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
 			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
 		#endif
 	} else {
-		text_layer_set_text(s_ampm_layer, _("error"));
-		text_layer_set_text(s_time_layer, _("error"));
+		text_layer_set_text(s_ampm_layer, "error");
+		text_layer_set_text(s_time_layer, "error");
 		#ifdef PBL_COLOR
 			window_set_background_color(s_main_window, GColorRed);
 			text_layer_set_text_color(s_ampm_layer, GColorWhite);
 			text_layer_set_text_color(s_time_layer, GColorWhite);
 			text_layer_set_text_color(s_date_layer, GColorWhite);
-			text_layer_set_text_color(s_temp_layer, GColorWhite);
-			text_layer_set_text_color(s_weather_layer, GColorWhite);
 		#else
 			window_set_background_color(s_main_window, GColorWhite);
 			text_layer_set_text_color(s_ampm_layer, GColorBlack);
 			text_layer_set_text_color(s_time_layer, GColorBlack);
 			text_layer_set_text_color(s_date_layer, GColorBlack);
-			text_layer_set_text_color(s_temp_layer, GColorBlack);
-			text_layer_set_text_color(s_weather_layer, GColorBlack);
 		#endif
 	}	
 
 	static char date_buffer[] = "WWWWWWWWW";
 
 	strftime(date_buffer, sizeof("WWWWWWWWW"), "%A", tick_time);
+	text_layer_set_text(s_date_layer, date_buffer);
 
-	if(strcmp(date_buffer, "Monday") == 0) {
-		text_layer_set_text(s_date_layer, _("Monday"));
+	// For localization
+	/*if (strcmp(date_buffer, "Monday") == 0) {
+		text_layer_set_text(s_date_layer, "Monday");
 	} else if(strcmp(date_buffer, "Tuesday") == 0) {
-		text_layer_set_text(s_date_layer, _("Tuesday"));
+		text_layer_set_text(s_date_layer, "Tuesday");
 	} else if(strcmp(date_buffer, "Wednesday") == 0) {
-		text_layer_set_text(s_date_layer, _("Wednesday"));
+		text_layer_set_text(s_date_layer, "Wednesday");
 	} else if(strcmp(date_buffer, "Thursday") == 0) {
-		text_layer_set_text(s_date_layer, _("Thursday"));
+		text_layer_set_text(s_date_layer, "Thursday");
 	} else if(strcmp(date_buffer, "Friday") == 0) {
-		text_layer_set_text(s_date_layer, _("Friday"));
+		text_layer_set_text(s_date_layer, "Friday"));
 	} else if(strcmp(date_buffer, "Saturday") == 0) {
-		text_layer_set_text(s_date_layer, _("Saturday"));
+		text_layer_set_text(s_date_layer, "Saturday");
 	} else if(strcmp(date_buffer, "Sunday") == 0) {
-		text_layer_set_text(s_date_layer, _("Sunday"));
+		text_layer_set_text(s_date_layer, "Sunday");
 	} else {
 		text_layer_set_text(s_date_layer, date_buffer);
-	}
-}
-
-static void tap_handler(AccelAxisType axis, int32_t direction) {
-	APP_LOG(APP_LOG_LEVEL_INFO, "Tap/flick registered!");
-
-	// Battery bar
-	GRect startbatt = GRect(-5,0,144,168);
-	GRect finishbatt = GRect(0,0,144,168);
-	animate_layer(batt_layer, &startbatt, &finishbatt, 1000, 0);
-
-	GRect startbatt2 = GRect(0,0,144,168);
-	GRect finishbatt2 = GRect(-5,0,144,168);
-	animate_layer(batt_layer, &startbatt2, &finishbatt2, 1000, 7000);
-
-	// Temp
-	GRect temps1 = GRect(288,0,141,168);
-	GRect temps2 = GRect(0,0,144,168);
-	animate_layer(text_layer_get_layer(s_temp_layer), &temps1, &temps2, 1000, 0);
-
-	GRect tempf1 = GRect(0,0,141,168);
-	GRect tempf2 = GRect(288,0,141,168);
-	animate_layer(text_layer_get_layer(s_temp_layer), &tempf1, &tempf2, 1000, 7000);
-
-	// Weather
-	GRect weathers1 = GRect(288,15,144,168);
-	GRect weathers2 = GRect(0,15,144,168);
-	animate_layer(text_layer_get_layer(s_weather_layer), &weathers1, &weathers2, 1000, 0);
-
-	GRect weatherf1 = GRect(0,15,144,168);
-	GRect weatherf2 = GRect(288,15,144,168);
-	animate_layer(text_layer_get_layer(s_weather_layer), &weatherf1, &weatherf2, 1000, 7000);
+	}*/
 }
 
 static void battery_handler(BatteryChargeState state) {
@@ -367,10 +272,8 @@ static void batt_layer_draw(Layer *layer, GContext *ctx) {
 static void main_window_load(Window *window) {
 	s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CYNTHE_25));
 	s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CYNTHE_22));
-	s_temp_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CYNTHE_18));
-	s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CYNTHE_14));
 
-	batt_layer = layer_create(GRect(-5, 0, 144, 168));
+	batt_layer = layer_create(GRect(0, 0, 144, 168));
 	
 	layer_set_update_proc(batt_layer, batt_layer_draw);
 
@@ -389,24 +292,10 @@ static void main_window_load(Window *window) {
 	text_layer_set_font(s_time_layer, s_time_font);
 	text_layer_set_text_alignment(s_time_layer, GTextAlignmentRight);
 
-	s_temp_layer = text_layer_create(GRect(288, 0, 141, 168));
-	text_layer_set_background_color(s_temp_layer, GColorClear);
-	text_layer_set_font(s_temp_layer, s_temp_font);
-	text_layer_set_text_alignment(s_temp_layer, GTextAlignmentRight);
-	text_layer_set_text(s_temp_layer, "Updating");
-
-	s_weather_layer = text_layer_create(GRect(288, 15, 141, 168));
-	text_layer_set_background_color(s_weather_layer, GColorClear);
-	text_layer_set_font(s_weather_layer, s_weather_font);
-	text_layer_set_text_alignment(s_weather_layer, GTextAlignmentRight);
-	text_layer_set_text(s_weather_layer, "Weather");
-
 	layer_add_child(window_get_root_layer(window), batt_layer);
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_ampm_layer));
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_temp_layer));
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
 
 	update_time();
 }
@@ -422,91 +311,8 @@ static void main_window_unload(Window *window) {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 	update_time();
-
-	// Update weather every 30 minutes
-	if(tick_time->tm_min % 30 == 0) {
-		// Begin dictionary
-		DictionaryIterator *iter;
-		app_message_outbox_begin(&iter);
-
-		// Add a key-value pair
-		dict_write_uint8(iter, 0, 0);
-
-		// Send the message!
-		app_message_outbox_send();
-	}
 }
 
-static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
-	// Store incoming information
-	static char temperature_buffer[25];
-	static char temperaturec_buffer[25];
-	static char conditions_buffer[100];
-
-	// Read first item
-	Tuple *t = dict_read_first(iterator);
-
-	// For all items
-	while(t != NULL) {
-		// Which key was received?
-		switch(t->key) {
-			case KEY_SCALE:
-				if (strcmp(t->value->cstring, "degf") == 0) { // If fahrenheit is selected
-					persist_write_bool(KEY_SCALE, false);
-				} else if (strcmp(t->value->cstring, "degc") == 0) { // If celsius is selected
-					persist_write_bool(KEY_SCALE, true);
-				} 
-			break;
-				case KEY_TEMPERATURE: // Temperature in fahrenheit
-					snprintf(temperature_buffer, sizeof(temperature_buffer), "Feels like %d", (int)t->value->int32);
-			break;
-				case KEY_TEMPERATUREC: // Temperature in celsius
-					snprintf(temperaturec_buffer, sizeof(temperaturec_buffer), "Feels like %d", (int)t->value->int32);
-			break;
-				case KEY_CONDITIONS:
-					snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
-			break;
-				default:
-					APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
-			break;
-		}
-
-		// Look for next item
-		t = dict_read_next(iterator);
-	}
-
-	// Set text for conditions
-	text_layer_set_text(s_weather_layer, conditions_buffer);
-
-	// Set text for temperature
-	bool scale = persist_read_bool(KEY_SCALE);
-
-	if (scale == false) {
-		text_layer_set_text(s_temp_layer, temperature_buffer); // Set to fahrenheit
-	} else if(scale == true) {
-		text_layer_set_text(s_temp_layer, temperaturec_buffer); // Set to celsius
-	} else {
-		text_layer_set_text(s_temp_layer, temperature_buffer); // Default to fahrenheit
-	}
-}
-	
-	
-
-static void inbox_dropped_callback(AppMessageResult reason, void *context) {
-	APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
-	text_layer_set_text(s_weather_layer, "Update Failed");
-	text_layer_set_text(s_temp_layer, "Update Failed");
-}
-
-static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
-	APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
-	text_layer_set_text(s_weather_layer, "Update Failed"); // No BT, Internet
-	text_layer_set_text(s_temp_layer, "Update Failed");
-}
-
-static void outbox_sent_callback(DictionaryIterator *iterator, void *context) { // updates weather
-	APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
-}
 
 static void init() {
 	s_main_window = window_create();
@@ -519,15 +325,6 @@ static void init() {
 	window_stack_push(s_main_window, true);
 	tick_timer_service_subscribe(HOUR_UNIT, tick_handler);
 	battery_state_service_subscribe(battery_handler);
-	accel_tap_service_subscribe(tap_handler);
-
-	// Register callbacks
-	app_message_register_inbox_received(inbox_received_callback);
-	// Open AppMessage
-	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-	app_message_register_inbox_dropped(inbox_dropped_callback);
-	app_message_register_outbox_failed(outbox_failed_callback);
-	app_message_register_outbox_sent(outbox_sent_callback);
 }
 
 static void deinit() {
@@ -538,7 +335,6 @@ static void deinit() {
 
 int main(void) {
 	init();
-	locale_init();
 	app_event_loop();
 	deinit();
 }
