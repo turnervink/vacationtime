45237c94e822d82506baac3f301e2f41a4cd047d
turnervink <turner.vink@me.com>
2015-08-20 11:57:07 -0700

    removed build from repo

--- build/c4che/_cache.py
+++
@@ -1,6 +0,0 @@
-BINDIR = '/usr/local/bin'
-DEFINES = ['RELEASE']
-LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/Users/Turner/pebble-dev/PebbleSDK-3.3/Pebble/common'
-PREFIX = '/usr/local'
-TARGET_PLATFORMS = [u'basalt', u'aplite']
