179f319a14c376dd6c5872c4d9379f3e1c9c6384
turnervink <turner.vink@me.com>
2015-08-24 11:44:37 -0700

    added functional battery bar added init animations removed unused fonts destroy new elements on unload

--- src/mealtime.c
+++ src/mealtime.c
@@ -6,6 +6,8 @@
 #define KEY_TEMPERATURE_IN_C 3
 #define KEY_CONDITIONS 4
 
+// check weather update is working every hour
+
 static Window *s_main_window;
 static TextLayer *s_time_layer, *s_ampm_layer, *s_date_layer, *s_temp_layer, *s_conditions_layer;
 static Layer *weather_layer, *batt_layer;
@@ -55,6 +57,19 @@ static void animate_layers() {
 	animate_layer(weather_layer, &finish, &start, 1000, 5000);
 }
 
+static void init_animation() {
+	GRect start = GRect(0, 175, 144, 168);
+	GRect first = GRect(0, 132, 142, 168);
+	GRect second = GRect(0, 115, 141, 168);
+	GRect third = GRect(0, 96, 142, 168);
+
+	int animlen = 700;
+
+	animate_layer(text_layer_get_layer(s_ampm_layer), &start, &third, animlen, 0);
+	animate_layer(text_layer_get_layer(s_date_layer), &start, &second, animlen, 0);
+	animate_layer(text_layer_get_layer(s_time_layer), &start, &first, animlen, 0);
+}
+
 static void set_text_colors(GColor color) {
 	text_layer_set_text_color(s_ampm_layer, color);
 	text_layer_set_text_color(s_time_layer, color);
@@ -67,6 +82,9 @@ static void batt_layer_draw(Layer *layer, GContext *ctx) {
 	time_t temp = time(NULL);
 	struct tm *tick_time = localtime(&temp);
 	int hour = tick_time->tm_hour;
+
+	BatteryChargeState state = battery_state_service_peek();
+ 	int pct = state.charge_percent;
 
 	if(hour >= 21) {
 		#ifdef PBL_COLOR
@@ -124,7 +142,8 @@ static void batt_layer_draw(Layer *layer, GContext *ctx) {
 		#endif
 	}
 
-	graphics_fill_rect(ctx, GRect(0, 0, 4, 168), 0, GCornerNone);
+	int totlen = 168;
+	graphics_fill_rect(ctx, GRect(0, 168, 3, -(totlen-(totlen*(100-pct))/100)), 0, GCornerNone);
 }
 
 static void update_time() {
@@ -229,8 +248,8 @@ static void update_time() {
 	strftime(date_buffer, sizeof("WWWWWWWWW"), "%A", tick_time);
 	text_layer_set_text(s_date_layer, date_buffer);
 
-	// For localization
-	/*if (strcmp(date_buffer, "Monday") == 0) {
+	/* For localization
+	if (strcmp(date_buffer, "Monday") == 0) {
 		text_layer_set_text(s_date_layer, "Monday");
 	} else if(strcmp(date_buffer, "Tuesday") == 0) {
 		text_layer_set_text(s_date_layer, "Tuesday");
@@ -306,19 +325,18 @@ static void main_window_load(Window *window) {
 	s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CYNTHE_25));
 	s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CYNTHE_22));
 	s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CYNTHE_14));
-	weather_layer = layer_create(GRect(144, 0, 144, 168));
 
-	s_ampm_layer = text_layer_create(GRect(0, 96, 142, 168));
+	s_ampm_layer = text_layer_create(GRect(0, 175, 144, 168));
 	text_layer_set_background_color(s_ampm_layer, GColorClear);
 	text_layer_set_font(s_ampm_layer, s_time_font);
 	text_layer_set_text_alignment(s_ampm_layer, GTextAlignmentRight);
 
-	s_date_layer = text_layer_create(GRect(0, 115, 141, 168));
+	s_date_layer = text_layer_create(GRect(0, 175, 144, 168));
 	text_layer_set_background_color(s_date_layer, GColorClear);
 	text_layer_set_font(s_date_layer, s_date_font);
 	text_layer_set_text_alignment(s_date_layer, GTextAlignmentRight);
 
-	s_time_layer = text_layer_create(GRect(0, 132, 142, 168));
+	s_time_layer = text_layer_create(GRect(0, 175, 144, 168));
 	text_layer_set_background_color(s_time_layer, GColorClear);
 	text_layer_set_font(s_time_layer, s_time_font);
 	text_layer_set_text_alignment(s_time_layer, GTextAlignmentRight);
@@ -332,6 +350,8 @@ static void main_window_load(Window *window) {
 	text_layer_set_background_color(s_conditions_layer, GColorClear);
 	text_layer_set_font(s_conditions_layer, s_weather_font);
 	text_layer_set_text_alignment(s_conditions_layer, GTextAlignmentRight);
+
+	weather_layer = layer_create(GRect(144, 0, 144, 168));
 
 	batt_layer = layer_create(GRect(-5, 0, 144, 168));
 	layer_set_update_proc(batt_layer, batt_layer_draw);
@@ -352,7 +372,7 @@ static void main_window_load(Window *window) {
 		use_celsius = persist_read_int(KEY_USE_CELSIUS);
 	}
 
-	update_layers();
+	update_layers(); // show/hide weather
 	update_time();
 }
 
@@ -360,23 +380,43 @@ static void main_window_unload(Window *window) {
 	text_layer_destroy(s_time_layer);
 	text_layer_destroy(s_ampm_layer);
 	text_layer_destroy(s_date_layer);
+	text_layer_destroy(s_temp_layer);
+	text_layer_destroy(s_conditions_layer);
+	layer_destroy(batt_layer);
+	layer_destroy(weather_layer);
 	fonts_unload_custom_font(s_time_font);
 	fonts_unload_custom_font(s_date_font);
+	fonts_unload_custom_font(s_weather_font);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 	update_time();
+
+	// Update weather every hour
+	if(tick_time->tm_min == 0) {
+		// Begin dictionary
+		DictionaryIterator *iter;
+		app_message_outbox_begin(&iter);
+
+		// Add a key-value pair
+		dict_write_uint8(iter, 0, 0);
+
+		// Send the message!
+		app_message_outbox_send();
+	}
 }
 
 static void tap_handler(AccelAxisType axis, int32_t direction) {
 	if (show_weather == 0) {
 		// do not animate weather_layer
 	} else {
-		animate_layers();
+		animate_layers(); // animate weather_layer
 	}
 
+	// animate battery bar
 	GRect startbatt = GRect(-5,0,144,168);
 	GRect finishbatt = GRect(0,0,144,168);
+
 	animate_layer(batt_layer, &startbatt, &finishbatt, 1000, 0);
 	animate_layer(batt_layer, &finishbatt, &startbatt, 1000, 5000);
 }
@@ -396,6 +436,8 @@ static void init() {
 
 	app_message_register_inbox_received(inbox_received_handler);
 	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+
+	init_animation(); // animate text layers on launch
 }
 
 static void deinit() {
